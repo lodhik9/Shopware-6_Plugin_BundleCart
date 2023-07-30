a0dca39fa60c2fcf9b2c216dcf033764c67461d2
Muhammad Khan Lodhi <lodhik9@gmail.com>
2023-07-30 10:51:17 +0200

    sixth commit

--- src/BundleExample.php
+++ src/BundleExample.php
@@ -35,6 +35,7 @@ class BundleExample extends Plugin {
         $connection->executeQuery('DROP TABLE IF EXISTS `swag_bundle_translation`');
         $connection->executeQuery('DROP TABLE IF EXISTS `swag_bundle`');
         $connection->executeQuery('ALTER TABLE `product` DROP COLUMN `bundles` ');
+        
 
     }
 
