ad3b25531dcac75b2d0c4251b37e919cbbb994f6
lodhik9 <lodhik9@gmail.com>
2023-08-05 14:12:35 +0200

    fifteenth commit

--- src/BundleExample.php
+++ src/BundleExample.php
@@ -17,7 +17,6 @@ class BundleExample extends Plugin {
         //TODO: Run InheritanceIndexer, tell indexer to index out out stuff
         $register = $this->container->get(EntityIndexerRegistry::class);
         $register->sendIndexingMessage(['product.indexer']);
-        
     }
     
     public function uninstall(UninstallContext $uninstallContext): void {
